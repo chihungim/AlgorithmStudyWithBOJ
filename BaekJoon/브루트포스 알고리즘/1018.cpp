19aedea5f205f77b7fc30f2e75b1695461f9e442
Neko kim <chihungim@gmail.com>
2021-12-13 00:43:21 +0900

    브루트포스 - 1018번 풀이 추가

--- BaekJoon/브루트포스 알고리즘/1018.cpp
+++ BaekJoon/브루트포스 알고리즘/1018.cpp
@@ -1,25 +1,63 @@
 #include <iostream>
-#include <array>
-
+#include <string>
 
 using namespace std;
 
+string board[51];
+
+string w_board[8] = { "WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW" };
+string b_board[8] = { "BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB" };
+
+int w_first(int x, int y)
+{
+	int r = 0;
+	for(int i = 0; i<8; i++)
+		for(int j = 0; j<8; j++)
+			if (board[x + i][y + j] != w_board[i][j])
+				r++;
+	
+	return r;
+}
+
+int b_first(int x, int y)
+{
+	int r = 0;
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			if (board[x + i][y + j] != b_board[i][j])
+				r++;
+		
+	
+	return r;
+}
+
 int main(int argc, char* argv[])
 {
-	array<array<char, 100>, 100> board{ {0,}, };
-	array<array<char, 8>, 8> chessboard{ {0,}, };
-	int n, m;
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	int n, m, min = 10000;
+
 	cin >> n >> m;
 
-	if(n == 8 && m == 8)
-	{
-		for(auto &row : chessboard)
-		{
-			for(auto& col : row)
-			{
-				cin >> col;
-			}
+	for (int i = 0; i < n; i++)
+		cin >> board[i];
+
+	int w = 0, b = 0;
+
+	for(int i = 0; i<=n - 8; i++ ){
+		for (int j = 0; j <= m - 8; j++) {
+
+			w = w_first(i, j);
+			b = b_first(i, j);
+
+			if (w < b)
+				min = (w < min) ? w : min;
+			else
+				min = (b < min) ? b : min;
 		}
 	}
-	
+
+	cout << min << "\n";
 }
