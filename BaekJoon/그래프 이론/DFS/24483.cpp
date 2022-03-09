e58ac9c4085250beb407bbc4013da5ea4aee2771
clgns rla <chihungim@gmail.com>
2022-03-10 08:30:04 +0900

    BOJ 24482 풀이추가 BOJ 24483 풀이추가

--- BaekJoon/그래프 이론/DFS/24483.cpp
+++ BaekJoon/그래프 이론/DFS/24483.cpp
@@ -2,20 +2,23 @@
 #include <algorithm>
 #include <vector>
 #include <functional>
+#define ll  long long
 
 using namespace std;
 
 vector<int> graph[100001];
-int _depth[100001] = { 0, };
+ll _depth[100001] = { 0, };
+ll _dis[100001] = { 0, };
 
 int main() {
 	fill(begin(_depth), end(_depth), -1);
-	
-	int n, m, r;
+
+	int n, m, r, order = 1;
 	cin >> n >> m >> r;
 
 	function<void(int, int)> dfs = [&](int cur, int depth) {
 		_depth[cur] = depth;
+		_dis[cur] = order++;
 		for (const auto& next : graph[cur]) {
 			if (_depth[next] == -1)
 				dfs(next, depth + 1);
@@ -28,13 +31,15 @@ int main() {
 		graph[u].push_back(v);
 	}
 
-	for (auto& g : graph) sort(g.begin(), g.end(), greater<int>());
-	
+	for (auto& g : graph) sort(g.begin(), g.end());
+
 	dfs(r, 0);
 
-	for (int i = 1; i <= n; i++) {
-		cout << _depth[i] << "\n";
-	}
+	ll sum = 0;
+	for (int i = 1; i <= n; i++)
+		sum += _dis[i] * _depth[i];
+	
+	cout << sum;
 
 	return 0;
 }
