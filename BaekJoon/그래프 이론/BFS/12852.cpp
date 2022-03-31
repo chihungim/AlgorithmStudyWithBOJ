2e898b3ec600f1004cb0d4889b3134e5654582a2
clgns rla <chihungim@gmail.com>
2022-03-31 09:00:37 +0900

    BOJ - 12852 풀이 추가

---
+++ BaekJoon/그래프 이론/BFS/12852.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct node
+{
+	node* from;
+	int value;
+};
+
+bool visited[1000001];
+int dist[1000001];
+
+int main(int argc, char* argv[])
+{
+	int x; cin >> x;
+	queue<node*> q;
+	node* dest = nullptr;
+
+	q.push(new node{ nullptr, x });
+	visited[x] = true;
+	vector<int> answer;
+
+	while(!q.empty())
+	{
+		const auto& from = q.front();
+		int _x = from->value;
+		q.pop();
+
+		if(_x == 1)
+		{
+			dest = from;
+			break;
+		}
+
+		if (_x < 1 || _x > 1000000) continue;
+
+		if (!visited[_x - 1])
+		{
+			visited[_x - 1] = true;
+			dist[_x - 1] = dist[_x] + 1;
+			q.push(new node{ from, _x - 1 });
+		}
+
+		if(_x%3 == 0 && !visited[_x/3])
+		{
+			visited[_x / 3] = true;
+			dist[_x / 3] = dist[_x] + 1;
+			q.push(new node{ from, _x / 3 });
+		}
+
+		if (_x % 2 == 0 && !visited[_x / 2])
+		{
+			visited[_x / 2] = true;
+			dist[_x / 2] = dist[_x] + 1;
+			q.push(new node{ from, _x / 2});
+		}
+	}
+
+	
+
+	while(dest != nullptr)
+	{
+		answer.push_back(dest->value);
+		dest = dest->from;
+	}
+
+	reverse(answer.begin(), answer.end());
+
+	cout << dist[1] << "\n";
+
+	for (auto& ans : answer)
+		cout << ans << " ";
+	return 0;
+}
