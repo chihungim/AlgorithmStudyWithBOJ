90b11e5c397c6c585cefa54e7d01cb77d528c636
김치훈 <chihungim@gmail.com>
2022-04-21 20:13:33 +0900

    boj 1439 풀이추가 boj 1786 풀이추가

--- BaekJoon/구현/1439.cpp
+++ BaekJoon/구현/1439.cpp
@@ -3,36 +3,29 @@
 
 using namespace std;
 
-
 //todo: Solve this
-
-bool isCOR(string& str) // contains one range
-{
-	int j1 = 0, j2 = 0;
-	bool f1 = true, f2 = false;
-
-	for(int i = 0; i<str.length(); i++)
-	{
-		if(f1 && str[i]== '1')
-		{
-			f1 = false;
-			j1 = i;
-		}
-
-		
-	}
-}
-
 
 int main()
 {
 	string str;
 	cin >> str;
-	//연속하는 숫자가 하나만 존재한다면?
+	int cnt = 0;
 
-	
+	while(true)
+	{
+		//연속하는 숫자가 하나만 존재한다면?
+		for(int i = 0; i<str.length(); i++)
+		{
+			if(i == str[i])
+			{
+				
+			}
+			else
+			{
+				
+			}
+		}
 
-
-	//연속하는 숫자가 아니라면?
-	
+		//연속하는 숫자가 아니라면?
+	}
 }
