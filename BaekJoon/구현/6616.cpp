0052f0c118c44b163282fc602ae0988117c172f4
김치훈 <chihungim@gmail.com>
2022-05-10 20:12:30 +0900

    qwer

---
+++ BaekJoon/구현/6616.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+void Transposition_cipher(string& origin, int &n) {
+
+}
+
+void trim() {
+
+}
+
+int main() {
+	int n;
+	string str;
+
+}
---
+++ BaekJoon/수학/24723.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int n;
+	cin >> n;
+	cout << pow(2,n);
+}
